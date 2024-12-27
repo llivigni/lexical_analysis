a77543b411cc7dcbea716a88fdecdcffab429e22
llivigni <158612465+llivigni@users.noreply.github.com>
2024-12-27 07:33:12 -0600

    Add files via upload

---
+++ front.h
@@ -0,0 +1,23 @@
+#ifndef FRONT_H
+#define FRONT_H
+
+/* Token types */
+typedef enum {
+    LETTER,         DIGIT,      UNKNOWN,
+    ASSIGN_OP,      ADD_OP,     KEY_IN,
+    LESSER_OP,      SUB_OP,     KEY_OUT,
+    GREATER_OP,     MULT_OP,    KEY_IF,
+    EQUAL_OP,       DIV_OP,     KEY_ELSE,
+    NEQUAL_OP,      MOD_OP,     OPEN_CURL,
+    LEQUAL_OP,      BOOL_AND,   CLOSE_CURL,
+    GEQUAL_OP,      BOOL_OR,    IDENT,
+    OPEN_PAREN,     BOOL_NOT,   INT_LIT,
+    CLOSE_PAREN,    SEMICOLON
+} Token;
+
+extern Token nextToken;
+
+int lex();
+
+#endif
+
